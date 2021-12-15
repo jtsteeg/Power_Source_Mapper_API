43ff45f03de9d4b560466d54902c837e6a6f7258
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-12-15 12:54:23 -0600

    adds endpoints to get and post a single power plant

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from azure.cosmos import CosmosClient, PartitionKey, exceptions
 import json
 import os
 from flask_marshmallow import Marshmallow
+import uuid
 
 
 app = Flask(__name__)
 
 
-#cosmos db data
+# cosmos db data
 url = os.environ['COSMOS_URI']
 key = os.environ['COSMOS_KEY']
 database_name = 'testPowerPlants'
@@ -23,27 +24,92 @@ container = database.get_container_client(container_name)
 
 # Query data
 query = "SELECT * from c"
-plants = list(container.query_items(
-    query=query,
-    enable_cross_partition_query=True
-))
 
 
 @app.route("/")
 def home():
     return "hello flask!"
 
+
 @app.route('/powerplants', methods=['GET'])
-def powerplants():
+def getPowerPlants():
+    plants = list(container.query_items(
+        query=query,
+        enable_cross_partition_query=True
+    ))
     result = plants_schema.dump(plants)
     return jsonify(result)
 
-#initialize serailizer
+
+@app.route('/powerplants/<name>', methods=['GET'])
+def getPowerPlantsByName(name):
+    print(name)
+    plants = list(container.query_items(
+        query=f'SELECT * from c WHERE lower(c.name)=lower(\'{name}\')',
+        enable_cross_partition_query=True
+    ))
+
+    if len(plants) == 0:
+        abort(404)
+
+    plant = plants[0]
+    result = plant_schema.dump(plant)
+    return jsonify(result)
+
+
+@app.route('/addpowerplants', methods=['POST'])
+def addNewPlant():
+    if not request.json:
+        abort(400)
+    powerPlant = request.json
+    powerPlantRecord = {**powerPlant, 'id': str(uuid.uuid4())}
+    print(powerPlantRecord)
+    matchingNames = list(container.query_items(
+        query=f'SELECT * from c WHERE lower(c.name)=lower(\'{powerPlantRecord["name"]}\')',
+        enable_cross_partition_query=True
+    ))
+    errors = []
+
+    if len(matchingNames) > 0:
+        errors.append('Duplicate names found')
+
+    matchingIds = list(container.query_items(
+        query=f'SELECT * from c WHERE lower(c.id)=lower(\'{powerPlantRecord["id"]}\')',
+        enable_cross_partition_query=True
+    ))
+
+    if len(matchingIds) > 0:
+        errors.append("Duplicate IDs found")
+
+    if powerPlantRecord['outputMWH'] < 0:
+        errors.append("outputMWH must be greater than 0")
+
+    if powerPlantRecord['coordinates']['lat'] > 90 or powerPlantRecord['coordinates']['lat'] < -90 :
+        errors.append("Latitude outside realistic range")
+    
+    if powerPlantRecord['coordinates']['lat'] > 90 or powerPlantRecord['coordinates']['lat'] < -90 :
+        errors.append("Latitude outside realistic range")
+
+    if len(errors) > 0:
+        response = jsonify({'errors': errors})
+        response.status_code = 400
+        return response
+
+    print(powerPlant)
+    dbResult = container.upsert_item(powerPlantRecord)
+    result = plant_schema.dump(dbResult)
+
+    return result
+
+
+# initialize serailizer
 ma = Marshmallow(app)
+
 
 class PlantSchema(ma.Schema):
     class Meta:
-        fields = ('name', 'coordinates', 'outputMWH', 'fuelTypes', 'Renewable')
+        fields = ('id', 'name', 'coordinates',
+                  'outputMWH', 'fuelTypes', 'Renewable')
 
 
 plant_schema = PlantSchema()
